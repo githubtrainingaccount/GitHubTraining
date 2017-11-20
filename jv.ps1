b6f97ba97ff166262fe8358fb26f39343c8da0da
manu.delcon <manu.delcon@acerta.be>
2017-11-20 10:15:53 +0000

    dfsdfssdf

--- jv.ps1
+++ jv.ps1
@@ -13,3 +13,35 @@ function FunctionName {
     General notes
     #>
 }
+
+function FunctionName {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
+}
+
+function FunctionName {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
+}
